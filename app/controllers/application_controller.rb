9701efb6d6a35ba214ffbfc453770eb90c5eb7df
ascii <ascii@192.168.43.205>
2014-01-23 17:51:07 +0800

    Modified before_filter to always check for validity of OAuth token New methods for JSON-parsed get and post methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,41 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :current_user
+  before_filter :set_user, :check_user
   
-  helper_method :current_user
+  helper_method :current_user, :current_token
   
   private
   
   def current_user
     session[:current_user_id]
+  end
+  
+  def current_token_session
+    @token_session
+  end
+  
+  def set_user
+    auth_hash = session[:auth_hash]
+    client = OAuth2::Client.new(DOORKEEPER_APP_ID, DOORKEEPER_APP_SECRET, :site => DOORKEEPER_APP_URL)
+    @token_session = OAuth2::AccessToken.from_hash(client, eval(auth_hash)) rescue nil
+  end
+  
+  def check_user
+    cs= current_token_session.get("/api/v1/check_session.json")
+    if cs.blank?
+      flash[:error] = "Your session has expired. Please login again."
+      redirect_to "/"
+    end
+  end
+  
+  def get_url(url)
+    response = current_token_session.get(url)
+    JSON.parse(response.body)
+  end
+  
+  def post_to_url(url, hash)
+    response = current_token_session.post(url, hash)
+    JSON.parse(response.body)
   end
 end
